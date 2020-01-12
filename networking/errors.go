5673304bce9727141b2edcf8f67f2ed9bef12be5
William <n.hua.drt@gmail.com>
2020-01-12 21:02:56 +0700

    fix return liteal ResponseError

--- networking/errors.go
+++ networking/errors.go
@@ -9,34 +9,34 @@ type ResponseError struct {
 	Status int    `json:"status"`
 }
 
-func NotFound(reason string) ResponseError {
-	return ResponseError{reason, http.StatusNotFound}
+func NotFound(reason string) *ResponseError {
+	return &ResponseError{reason, http.StatusNotFound}
 }
 
-func Forbidden(reason string) ResponseError {
-	return ResponseError{reason, http.StatusForbidden}
+func Forbidden(reason string) *ResponseError {
+	return &ResponseError{reason, http.StatusForbidden}
 }
 
-func Conflict(reason string) ResponseError {
-	return ResponseError{reason, http.StatusConflict}
+func Conflict(reason string) *ResponseError {
+	return &ResponseError{reason, http.StatusConflict}
 }
 
-func BadRequest(reason string) ResponseError {
-	return ResponseError{reason, http.StatusBadRequest}
+func BadRequest(reason string) *ResponseError {
+	return &ResponseError{reason, http.StatusBadRequest}
 }
 
-func UnprocessableRequest(reason string) ResponseError {
-	return ResponseError{reason, statusUnprocessableEntity}
+func UnprocessableRequest(reason string) *ResponseError {
+	return &ResponseError{reason, statusUnprocessableEntity}
 }
 
-func UnauthorizedRequest(reason string) ResponseError {
-	return ResponseError{reason, http.StatusUnauthorized}
+func UnauthorizedRequest(reason string) *ResponseError {
+	return &ResponseError{reason, http.StatusUnauthorized}
 }
 
-func InternalServerError(reason string) ResponseError {
-	return ResponseError{reason, http.StatusInternalServerError}
+func InternalServerError(reason string) *ResponseError {
+	return &ResponseError{reason, http.StatusInternalServerError}
 }
 
-func CustomError(reason string, status int) ResponseError {
-	return ResponseError{reason, status}
+func CustomError(reason string, status int) *ResponseError {
+	return &ResponseError{reason, status}
 }
